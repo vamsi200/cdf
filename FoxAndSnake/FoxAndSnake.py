49763baca05f6ac6a8b47dcc7f66fbd31f54b2fc
vamsi200 <vamsivanama891@gmail.com>
2025-02-22 20:31:44 +0530

    more

--- FoxAndSnake/FoxAndSnake.py
+++ FoxAndSnake/FoxAndSnake.py
@@ -1,10 +1,10 @@
-n, m = map(int , input().split())
+n, m = map(int, input().split())
 
 for i in range(n):
-    if i % 4 == 0:
-        print('#'*m)
-    elif i % 4 == 2:
-        print('#' + '.' * (m-1))
+    if i % 2 == 0:
+        print('#' * m)
+    elif i % 4 == 1:
+        print('.' * (m - 1) + '#')
     else:
-        print('.'*(m-1) + '#')
+        print('#' + '.' * (m - 1))
 
